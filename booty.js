79d7f308b7e430b6bc9d3c2d288a3e4701477d95
Alex Davis <davis.alex.jr@gmail.com>
2017-07-15 13:33:33 -0400

    Made a dice roll command

--- booty.js
+++ booty.js
@@ -1,15 +1,23 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const settings = require('settings.json');
+// const Discord = require('discord.js');
+const commando = require('discord.js-commando');
+const client = new commando.Client();
 
+client.registry.registerGroup('random', 'Random');
+client.registry.registerDefaults();
+client.registry.registerCommandsIn(__dirname + "/commands");
+
+//login bot using token
+client.login('MzM1MjY2MzQ1NDg5MDA2NTky.DEnQzg.q6cxlMa_1bdaerOQzHbzTbN00sQ');
+
+//ready message when bot starts
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
-client.on('message', message => {
-  if (message.content === 'poopy') {
-    message.reply('pants');
-  }
-});
 
-client.login('settings.token');
+//reply to user when user types poopy
+client.on('message', (message) => {
+    if(message.content == 'poopy'){
+        message.reply('pants');
+    }
+});

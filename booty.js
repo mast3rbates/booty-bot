a3a4bed47f8e125eedcf38cd0424fbd25e422d43
Alex Davis <davis.alex.jr@gmail.com>
2017-07-16 11:57:05 -0400

    added unfun licence and readme

--- booty.js
+++ booty.js
@@ -14,12 +14,30 @@ client.on('ready', () => {
   console.log('I am ready!');
 });
 
+var regexMeme = /memes|meme/ig;
+var regexUplay = /uplay/ig;
+
+var uplayReplies = ["Uplay is shit.",
+                    "Uplay couldn't follow through making a good game even with divine intervention.",
+                    "Uplay is a bottom feeding piece of shit",
+                    "https://i.redditmedia.com/0uaia3fh5l3OqZ-OmzWd6UO5ZgJ-s1BtGT904qxbiwY.jpg?w=778&s=67f1fce2921045739bb2e331bbefe522",
+                    "[ACTION COMPLETED]",
+                    "\"honhonhonhonhon\" -Uplay probably",
+                    "You are not connected to the Ubisoft servers right now, would you like to reconnect to continue discussing Uplay?"]
+
 client.on('message', (message) => {
+
     if(message.content == 'poopy'){
         message.channel.send('pants');
     }
 
-    if(message.content == 'meme' || message.content == 'memes'){
+    if(regexMeme.test(message.content)){
         message.channel.send('Oh, I love memes!');
     }
+
+    var randomUreply = Math.floor(Math.random()*uplayReplies.length);
+    if(regexUplay.test(message.content)){
+        message.channel.send(uplayReplies[randomUreply]);
+    }
+
 });

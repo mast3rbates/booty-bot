b949a721508577f11868b5063a9c550c4a38e076
mast3rbates <conprophage@gmail.com>
2017-07-16 14:03:27 -0700

    Update booty.js

--- booty.js
+++ booty.js
@@ -51,11 +51,6 @@ client.on('message', (message) => {
     //     message.channel.send('<@151840868079370241>');
     // }
 
-    //link to mission template download
-    if(message.content == '#missiontemplate'){
-        message.channel.send('https://github.com/CookieDefender/TEST-Mission-Template/releases/');
-    }
-
     //when memes are mentioned send "I love memes"
     if(regexMeme.test(message.content)){
         message.channel.send('Oh, I love memes!');
